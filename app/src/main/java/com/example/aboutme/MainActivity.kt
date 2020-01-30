e15e38d90b8bd4a787db1c6717d08d5b950b2b5c
watanabe <momossy5>
2020-01-30 16:20:29 +0900

    Use data binding to display data

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -5,36 +5,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import com.example.aboutme.databinding.ActivityMainBinding
 import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private val myName: MyName = MyName("Aleks Haecky")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding.doneButton.setOnClickListener { addNickname(it) }
-        binding.nicknameText.setOnClickListener { updateNickname(it) }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            myName = myName
+            doneButton.setOnClickListener { addNickname(it) }
+            nicknameText.setOnClickListener { updateNickname(it) }
+        }
     }
 
     private fun addNickname(view: View) {
         binding.apply {
-            nicknameText.text = binding.nicknameEdit.text.toString()
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
         }
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+        val inputMethodManager =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
-    private fun updateNickname (view: View) {
+    private fun updateNickname(view: View) {
         val editText = binding.nicknameEdit
         val doneButton = binding.doneButton
         editText.visibility = View.VISIBLE
